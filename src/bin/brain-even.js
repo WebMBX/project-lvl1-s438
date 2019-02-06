0f356845357000e628b029418fde71c8c3f353e5
Vano <webmbx@yandex.ru>
2019-02-06 14:29:03 +0300

    new version 1.2.2 (fix all eslint errors and refactoring)

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
-import { askName, game } from '..';
+import { askName, gameQuestion, sayHi } from '..';
 
-console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if number even otherwise answer "no".');
-console.log('');
-const name = askName();
-
-game(1, name);
+sayHi();
+gameQuestion(1, askName());
--- src/index.js
+++ src/index.js
@@ -1,26 +1,37 @@
 import readlineSync from 'readline-sync';
 
+export const sayHi = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  console.log('');
+};
+
 export const askName = () => {
   const name = readlineSync.question('What is Your name, cowboy? ');
   console.log(`Let's go ${name}!`);
   return name;
 };
 
-const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
+const getRandomInt = (a, b) => {
+  const min = Math.ceil(a);
+  const max = Math.floor(b);
+  return Math.abs(Math.floor(Math.random() * (min - max)) + min);
+};
 
-export const game = (n, name) => {
-  if (n === 4) {
+export const gameQuestion = (n, name) => {
+  if (n > 3) {
     console.log('');
     return console.log(`Congratulations, ${name}!`);
   }
-  const number = getRandomInt(50);
+  const number = getRandomInt(10, 60);
   console.log(`Question: ${number}`);
   const answer = readlineSync.question('Your answer: ');
   const correct = (number % 2 === 0) ? 'yes' : 'no';
   if ((number % 2 === 0 && answer === correct) || (number % 2 !== 0 && answer === correct)) {
     console.log('Correct!');
-    game(n + 1, name);
+    gameQuestion(n + 1, name);
   } else {
-    return console.log(`Your answer is wrong ;(. Correct answer was ${correct}. Let's try again, ${name}!`);
+    return console.log(`Your answer '${answer}' is wrong ;(. Correct answer was '${correct}'. Let's try again, ${name}!`);
   }
+  return false;
 };
